08d71a83b1d7a60246d1adc7281c75061abdb0fb
ThomasPascual <thoma1@free.fr>
2021-07-27 09:52:33 +0200

    creer son profil et le modifier

---
+++ src/Controller/ProfileController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Profile;
+use App\Form\ProfileType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class ProfileController extends AbstractController
+{
+    #[Route('/client/profile', name: 'profile')]
+    public function afficher(): Response
+    {
+        return $this->render('profile/profile.html.twig', [
+            'user' => $this->getUser()
+        ]);
+    }
+
+    #[Route('/client/profile/nouveau', name: 'profile_new')]
+    public function creation(Request $request, EntityManagerInterface $em): Response
+    {
+        $user = $this->getUser();
+        $profile = new Profile();
+        $form = $this->createForm(ProfileType::class, $profile);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user->setProfile($profile);
+            $em->persist($user);
+            $em->flush();
+            return $this->redirectToRoute('profile');
+        }
+
+        return $this->render('profile/creationProfile.html.twig', [
+          //  'user' => $user,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/client/profile/{id}', name: 'profile_edit')]
+    public function modification(Profile $profile, Request $request, EntityManagerInterface $em): Response
+    {
+        $user = $this->getUser();
+        $form = $this->createForm(ProfileType::class, $profile);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user->setProfile($profile);
+            $em->persist($user);
+            $em->flush();
+            return $this->redirectToRoute('profile');
+        }
+
+        return $this->render('profile/creationProfile.html.twig', [
+            //'user' => $user,
+            'form' => $form->createView(),
+        ]);
+    }
+
+
+    
+
+
+    // #[Route('/admin/creation', name: 'creationVoiture')]
+    // #[Route('/admin/{id}', name: 'modifVoiture', methods: ['GET', 'POST'])]
+    // public function modification(Voiture $voiture = null, Request $request, EntityManagerInterface $em)
+    // {
+    //     if (!$voiture) {
+    //         $voiture = new Voiture();
+    //     }
+    //     $form = $this->createForm(VoitureType::class, $voiture);
+    //     $form->handleRequest($request);
+    //     if ($form->isSubmitted() && $form->isValid()) {
+    //         $modif = $voiture->getId() !== null;
+    //         $em->persist($voiture);
+    //         $em->flush();
+    //         $this->addFlash('success', ($modif) ? "La modification a été effectuée" : "l'ajout a été effectué");
+    //         return $this->redirectToRoute('admin');
+    //     }
+    //     return $this->render('admin/modification.html.twig', [
+    //         'voiture' => $voiture,
+    //         'form' => $form->createView(),
+    //     ]);
+    // }
+}
---
+++ src/Form/ProfileType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Profile;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProfileType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nom')
+            ->add('prenom')
+            ->add('ville')
+            ->add('code_postal')
+            ->add('age')
+            ->add('image_profile')
+            //->add('user')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Profile::class,
+        ]);
+    }
+}
