93c884bebf9811566db9f5a93ff0d7dba7b24acd
ThomasPascual <thoma1@free.fr>
2021-07-30 10:08:39 +0200

    ajoute le prix total en bdd lorsque l on fait une reservation

--- src/Controller/LocationController.php
+++ src/Controller/LocationController.php
@@ -20,9 +20,16 @@ class LocationController extends AbstractController
         $location = new Location;
         $form = $this->createForm(LocationType::class, $location);
         $form->handleRequest($request);
+
+        //calcul nombre de jours à mettre dans un service
+        $debut = $form->get('debut')->getData();
+        $fin = $form->get('fin')->getData();
+        $jours = ($fin->diff($debut)->format("%a")) + 1;
+
         if ($form->isSubmitted() && $form->isValid()) {
             $location->setUser($user)
-                    ->setVoiture($voiture) 
+                    ->setVoiture($voiture)
+                    ->setPrix($voiture->getModele()->getPrixMoyen() * $jours) //entre le prix total de la location
                     ->setDateCreation(new \DateTime()); //on set l'user actuel et la voiture choisit qu'on a recuppéré grâce à l'id de la voiture.
             $em->persist($location);
             $em->flush();
