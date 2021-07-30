0ea99b703e52b6b81bbdab524d7543c7f8bd3d89
ThomasPascual <thoma1@free.fr>
2021-07-30 12:19:49 +0200

    affiche l annee a partir de la date d ajd pour les reservation

--- src/Form/LocationType.php
+++ src/Form/LocationType.php
@@ -6,18 +6,23 @@ use App\Entity\Location;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+
 
 class LocationType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('debut')
-            ->add('fin')
-           // ->add('date_creation')
-           // ->add('prix')
-            //->add('user')
-           // ->add('voiture')
+            ->add('debut', DateType::class, [
+                'widget' => 'choice',
+                'years' => range(date('Y'), date('Y') + 10),
+                //'format' => 'yyyy-MM-dd',
+            ])
+            ->add('fin', DateType::class, [
+                'widget' => 'choice',
+                'years' => range(date('Y'), date('Y') + 10),
+            ])
         ;
     }
 
