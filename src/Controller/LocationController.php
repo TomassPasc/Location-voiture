6204df01fa51ed620b6010c4c71fe5ab004fbb13
ThomasPascual <thoma1@free.fr>
2021-08-02 12:15:05 +0200

    ajout d un envoie de mail lorsque la reservation est faite plus message flash

--- src/Controller/LocationController.php
+++ src/Controller/LocationController.php
@@ -7,6 +7,7 @@ use App\Entity\Location;
 use App\Form\LocationType;
 use App\Form\ReservationType;
 use App\Service\CalculService;
+use App\Service\MailerService;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -16,30 +17,48 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class LocationController extends AbstractController
 {
     #[Route('/client/location/{id}', name: 'location')]
-    public function index(Voiture $voiture, Request $request, EntityManagerInterface $em, CalculService $calculService): Response
+    public function index(Voiture $voiture, Request $request, EntityManagerInterface $em, CalculService $calculService, MailerService $mailer): Response
     {
          $session = $request->getSession();
          $reservations = $session->get('reservations');
-        // dd($reservations['date_debut']);
-       
+  
         $user = $this->getUser(); 
+   
         $location = new Location;
         $form = $this->createForm(ReservationType::class, $location);
         $form->handleRequest($request);
         
         if ($form->isSubmitted() && $form->isValid()) {
-           // dd($voiture);
-            //calcul nombre de jours à mettre dans un service
+
+            //calcul avec le service:
             $jours = $calculService->nombreJours($reservations['date_debut'], $reservations['date_fin']);
+
+            //on ajoute toutes les données nécessaire pour la location
             $location->setUser($user)
                     ->setVoiture($voiture)
                     ->setDebut($reservations['date_debut'])
                     ->setFin($reservations['date_fin'])
                     ->setPrix($voiture->getModele()->getPrixMoyen() * $jours) //entre le prix total de la location
                     ->setDateCreation(new \DateTime()); //on set l'user actuel et la voiture choisit qu'on a recuppéré grâce à l'id de la voiture.
-
+      
             $em->persist($location);
             $em->flush();
+
+            //message pour le retour à la page principal
+            $this->addFlash('success', "Votre réservation a bien été effectué");
+
+
+            $mailer->send(
+                "thoma1@free.fr",
+                $user->getEmail(),
+                'votre réservation de location',
+                "location/mail.html.twig",
+                [
+                    'location' => $location
+                ]
+            );
+
+
             return $this->redirectToRoute('voitures');
         }
 
