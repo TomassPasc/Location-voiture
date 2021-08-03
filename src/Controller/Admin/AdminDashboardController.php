0d8e447c55de5f32cc5f7d4a9bbaad6931570ecb
ThomasPascual <thoma1@free.fr>
2021-08-03 16:11:18 +0200

    ajout des reservations au dashboard

--- src/Controller/Admin/AdminDashboardController.php
+++ src/Controller/Admin/AdminDashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Admin;
 
+use App\Repository\LocationRepository;
 use App\Service\StatsService;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,13 +12,20 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class AdminDashboardController extends AbstractController
 {
     #[Route('/admin', name: 'admin_dashboard')]
-    public function index(EntityManagerInterface $manager, StatsService $statsService)
+    public function index(EntityManagerInterface $manager,   StatsService $statsService)
     {
 
         $stats = $statsService->getStats();
+        $locationsFuture = $statsService->getLocationFuturStats();
+        $locationsPasse = $statsService->getLocationPasseStats();
+        $locationActuelle = $statsService->getLocationPresenteStats();
+    
 
         return $this->render('admin/dashboard/index.html.twig', [
             'stats' => $stats,
+            'locFuture' => $locationsFuture,
+            'locAct' => $locationActuelle,
+            'locPasse' =>$locationsPasse
         ]);
     }
 }
--- src/Service/StatsService.php
+++ src/Service/StatsService.php
@@ -4,14 +4,17 @@ namespace App\Service;
 
 use Doctrine\ORM\EntityManagerInterface;
 
-class StatsService {
+class StatsService
+{
     private $manager;
 
-    public function __construct(EntityManagerInterface $manager) {
+    public function __construct(EntityManagerInterface $manager)
+    {
         $this->manager = $manager;
     }
 
-    public function getStats() {
+    public function getStats()
+    {
         $users      = $this->getUsersCount();
         $voitures        = $this->getVoituresCount();
         $locations   = $this->getLocationsCount();
@@ -20,15 +23,18 @@ class StatsService {
         return compact('users', 'voitures', 'locations');
     }
 
-    public function getUsersCount() {
+    public function getUsersCount()
+    {
         return $this->manager->createQuery('SELECT COUNT(u) FROM App\Entity\User u')->getSingleScalarResult();
     }
 
-    public function getVoituresCount() {
+    public function getVoituresCount()
+    {
         return $this->manager->createQuery('SELECT COUNT(v) FROM App\Entity\Voiture v')->getSingleScalarResult();
     }
 
-    public function getLocationsCount() {
+    public function getLocationsCount()
+    {
         return $this->manager->createQuery('SELECT COUNT(l) FROM App\Entity\Location l')->getSingleScalarResult();
     }
 
@@ -36,17 +42,41 @@ class StatsService {
     //     return $this->manager->createQuery('SELECT COUNT(c) FROM App\Entity\Comment c')->getSingleScalarResult();
     // }
 
-    // public function getAdsStats($direction) {
-    //     return $this->manager->createQuery(
-    //         'SELECT AVG(c.rating) as note, a.title, a.id, u.firstName, u.lastName, u.picture
-    //         FROM App\Entity\Comment c 
-    //         JOIN c.ad a
-    //         JOIN a.author u
-    //         GROUP BY a
-    //         ORDER BY note ' . $direction
-    //     )
-    //     ->setMaxResults(5)
-    //     ->getResult();
-    // }
+    public function getLocationFuturStats()
+    {
+        return $this->manager->createQuery(
+            'SELECT l
+            FROM App\Entity\Location l 
+            WHERE l.debut > :now
+            ORDER BY l.debut'
+        )
+            ->setParameter('now', new \DateTime('now'))
+            ->getResult();
+    }
 
+    public function getLocationPasseStats()
+    {
+        return $this->manager->createQuery(
+            'SELECT l
+            FROM App\Entity\Location l 
+            WHERE l.fin < :now
+            ORDER BY l.debut DESC'
+        )
+            ->setParameter('now', new \DateTime('now'))
+            ->getResult();
+    }
+
+    public function getLocationPresenteStats()
+    {
+
+
+        return $this->manager->createQuery(
+            'SELECT l
+            FROM App\Entity\Location l 
+            WHERE :now BETWEEN l.debut AND l.fin
+            ORDER BY l.debut'
+        )->setParameter('now', new \DateTime('now'))
+        ->getResult();
+
+    }
 }
